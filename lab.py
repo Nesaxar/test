8f8ec7c6ae7fe1a63f4f1f6cd5f51e0103fafd4d
Nesaxar <dmutrodima@gmail.com>
2021-10-28 16:42:02 +0300

    lab1

--- lab.py
+++ lab.py
@@ -1,1 +1,24 @@
-print("hello world")
+action=(input("enter action"))
+numeric_one=int(input("print numeric"))
+
+numeric_too=int(input("print numeric"))
+if action == "+""*""-""//""%":
+    print(numeric_one+numeric_too, )
+elif action=="//":
+    print(numeric_one//numeric_too)
+elif action=="*":
+    print(numeric_one*numeric_too)
+elif action=="%":
+    print(numeric_one%numeric_too)
+elif action=="-":
+    print(numeric_one-numeric_one)
+else:
+    print(numeric_one-numeric_too)
+
+
+
+
+
+
+
+
---
+++ lab5.py
@@ -0,0 +1,6 @@
+hours=int(input(   'години'))
+b=(hours*60)
+if hours<=24:
+    print(b,"хвилин")
+elif hours>=25:
+    print("erorr")
