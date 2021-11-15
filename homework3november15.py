b1d25b45cc5e0b5c487d6ead42b5ae02b1272941
Nesaxar <92783783+Nesaxar@users.noreply.github.com>
2021-11-15 15:36:16 +0200

    Add files via upload

---
+++ homework3november15.py
@@ -0,0 +1,4 @@
+user_input = input("введіть що не будь:")
+
+a_list =  list(user_input.split(','))
+print(a_list)
---
+++ homeworknovember15.py
@@ -0,0 +1,3 @@
+word=['Name','first name','Last name']
+for i in reversed (word):
+    print(i)
